dd3e2474649c9ac9fea8a46a49cd0142d80ae517
dl471 <annamika@bk.ru>
2018-10-17 19:24:21 +0100

    Add ActiveEncodings
    -Provides an easily accessible indicator of the current encodings

---
+++ KPT/ActiveEncodings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace KPT
+{
+    static class ActiveEncodings
+    {
+        public static Encoding currentEncoding;
+
+        public static void Initalize()
+        {
+            currentEncoding = Encoding.GetEncoding("shift-jis");
+        }
+
+    }
+
+}

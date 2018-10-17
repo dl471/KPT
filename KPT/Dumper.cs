d73908082588b729c886a2beb667e177b9e6e563
dl471 <annamika@bk.ru>
2018-10-17 12:42:34 +0100

    Create Dumper class
    -Implement recursive directory searching for Dumper

---
+++ KPT/Dumper.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace KPT
+{
+    class Dumper
+    {
+
+        private const string originalDirectory = "Original";
+        private const string editableDirectory = "Editable";
+
+        struct FileLocationMeta
+        {
+            public string leadingPath;
+            public string switchPath;
+            public string subPath;
+            public string fileName;       
+            
+            public FileLocationMeta(string leadingPath, string switchPath, string subPath, string fileName)
+            {
+                this.leadingPath = leadingPath;
+                this.switchPath = switchPath;
+                this.subPath = subPath;
+                this.fileName = fileName;
+            }
+                     
+        }
+
+        /// <summary>
+        /// Check if the directory is valid, that is, that it exists and that it passes any other tests specified in remarks
+        /// </summary>
+        /// <param name="targetDirectoryPath">The path of the directory to be checked</param>
+        /// <returns>True if valid, false if invalid</returns>
+        /// <remarks>
+        /// Current tests are:
+        /// - Directory exists
+        /// - Directory contains a "PSP_GAME" folder
+        /// </remarks>
+        private bool CheckDirectoryValidity(string targetDirectoryPath)
+        {
+            if (!Directory.Exists(targetDirectoryPath))
+            {
+                string errorMessage = string.Format("Could not find directory {0}. Please check that the path is correct and that the directory exists.", targetDirectoryPath);
+                MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            var initialDirectoryListing = Directory.GetDirectories(targetDirectoryPath);
+            var initialDirectoryContents = new HashSet<string>();
+
+            foreach (var dir in initialDirectoryListing)
+            {
+                initialDirectoryContents.Add(Path.GetFileName(dir));
+            }
+
+            if (!initialDirectoryContents.Contains("PSP_GAME"))
+            {
+                string errorMessage = string.Format("Directory {0} did not match the expected format. Expected a directory containing a \"PSP_GAME\" directory.", targetDirectoryPath); // this is to nudge people in the right path and be disabled or customized if necessary should another format come into play
+                MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Go through a directory and split it into an "Original" directory with a copy of the original files and an "Editble" directory with processed files that can be edited by users
+        /// </summary>
+        /// <param name="targetDirectoryPath">The path of the directory to be processed</param>
+        /// <returns>True is succesfully processed, false if processing failed</returns>
+        public bool ProcessDirectory(string targetDirectoryPath)
+        {
+            
+            if (!CheckDirectoryValidity(targetDirectoryPath))
+            {
+                return false;
+            }
+
+            List<FileLocationMeta> fileList = new List<FileLocationMeta>();
+
+            PopulateFileList(targetDirectoryPath, fileList);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Iterate recursively through a given directory and subdirectory and fill the provided list with information on all files contained within
+        /// </summary>
+        /// <param name="leadingPath">The directory to start from</param>
+        /// <param name="fileList">The file list which should be populated</param>
+        private void PopulateFileList(string leadingPath, List<FileLocationMeta> fileList)
+        {
+            PopulateFileList(leadingPath, "", fileList);
+        }
+
+        /// <summary>
+        /// Iterate recursively through a given directory and subdirectory and fill the provided list with information on all files contained within
+        /// </summary>
+        /// <param name="leadingPath">The directory to start from</param>
+        /// <param name="subPath">Used to keep track of the current subdirectory that is being iterated though (usually set to "" initially)</param>
+        /// <param name="fileList">The file list which should be populated</param>
+        private void PopulateFileList(string leadingPath, string subPath, List<FileLocationMeta> fileList)
+        {
+            string currentAbsolutePath = Path.Combine(leadingPath, subPath);
+            var subdirectories = Directory.GetDirectories(currentAbsolutePath);
+            
+            foreach (var subdirectory in subdirectories)
+            {
+                string dirWithoutPath = Path.GetFileName(subdirectory);
+                string newSubPath = Path.Combine(subPath, dirWithoutPath);
+                PopulateFileList(leadingPath, newSubPath, fileList);
+            }
+
+            var currentDirectoryFiles = Directory.GetFiles(currentAbsolutePath);
+
+            foreach (var file in currentDirectoryFiles)
+            {
+                string filenameWithoutPath = Path.GetFileName(file);
+                FileLocationMeta newFileData = new FileLocationMeta();
+                newFileData.leadingPath = leadingPath;
+                newFileData.subPath = subPath;
+                newFileData.fileName = filenameWithoutPath;
+                fileList.Add(newFileData);
+            }
+
+        }
+
+    }
+}
--- KPT/Program.cs
+++ KPT/Program.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Text;
+using LibCPK;
 
 namespace KPT
 {
+
     static class Program
     {
         /// <summary>
@@ -16,6 +20,39 @@ namespace KPT
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+
+            const string testFileDirectory = @"C:\Sandbox\kay\desubox\drive\H\kpt_test\test files";
+            string testFileLocation = Path.Combine(testFileDirectory, "test_file_1.cpk");
+            string testInLocation = Path.Combine(testFileDirectory, "test_in");
+            string testOutLocation = Path.Combine(testFileDirectory, "test_out");
+
+            string testDirectory = @"C:\Sandbox\kay\desubox\drive\H\kpt_test\test_in";
+
+            var dumpTest = new Dumper();
+            dumpTest.ProcessDirectory(testDirectory);
+                
+
+
+            var test = new CPK(new Tools());
+
+            Encoding sj = Encoding.GetEncoding("shift-jis");
+
+
+            foreach (FileEntry file in test.FileTable)
+            {
+                string fileName = file.FileName.ToString();
+                MessageBox.Show(fileName);
+                if (fileName.EndsWith(".bin"))
+                {
+                    int filePos = (int)(file.FileOffset);
+                    uint fileSize = (uint)(file.FileSize);
+                    MessageBox.Show(fileSize.ToString("X"));
+                }
+            }
+
+            Environment.Exit(0);
+
             Application.Run(new Form1());
         }
     }

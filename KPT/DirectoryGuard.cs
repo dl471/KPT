177aaf207c0730614ac37ef0c896a2767ea669b7
dl471 <annamika@bk.ru>
2018-10-17 19:22:50 +0100

    Add DirectoryGuard
    -Provides functionality to ensure that all directories in a path exist

---
+++ KPT/DirectoryGuard.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace KPT
+{
+
+    /// <summary>
+    /// Used to guard against attempting to create files in non-existent directories
+    /// </summary>
+    static class DirectoryGuard
+    {
+        /// <summary>
+        /// A collection of directories already known to exist by the DirectoryGuard
+        /// </summary>
+        static HashSet<string> knownDirectories;
+
+        public static void Initalize()
+        {
+            knownDirectories = new HashSet<string>();
+        }
+
+        /// <summary>
+        /// Ensures that every directory in the given path exists. Creates them if they do not.
+        /// </summary>
+        /// <param name="directory">The directory to be checked</param>
+        public static void CheckDirectory(string directory)
+        {
+            string targetDir = Path.GetDirectoryName(directory);
+            
+            while (!knownDirectories.Contains(targetDir) && !Directory.Exists(targetDir))
+            {
+                Directory.CreateDirectory(targetDir);
+                knownDirectories.Add(targetDir);
+                CheckDirectory(targetDir);
+            }
+
+        }
+
+    }
+}

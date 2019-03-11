582599117ed471bcc9d264cd77ebb89ef1949469
dl471 <annamika@bk.ru>
2019-03-11 21:49:37 +0000

    Fix version

--- KPT/Version.cs
+++ KPT/Version.cs
@@ -16,12 +16,14 @@ namespace KPT
             beta_0_0_1,
             beta_1_0_0,
             beta_1_0_1,
+            beta_1_1_0,
+            beta_1_1_1,
         };
 
         /// <summary>
         /// Current version
         /// </summary>
-        const Versions CURRENT_VERSION = Versions.beta_1_0_1;
+        const Versions CURRENT_VERSION = Versions.beta_1_1_1;
 
         public static Versions CurrentVersion
         {

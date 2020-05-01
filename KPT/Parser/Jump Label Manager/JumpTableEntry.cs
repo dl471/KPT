26ed27b9bd6602ea6c7756a203df665f02ee15d3
dl471 <annamika@bk.ru>
2020-05-01 04:07:52 +0100

    Basic outline of jump table management

---
+++ KPT/Parser/Jump Label Manager/JumpTableEntry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using KPT.Parser.Elements;
+
+namespace KPT.Parser.Jump_Label_Manager
+{
+    /// <summary>
+    /// Marks an entry in the games jump table
+    /// </summary>
+    class JumpTableEntry : IElement
+    {
+
+        StCpNumber fileNumber;
+        DataBox firstBlock; // this block contains stuff known to be related to the jumps and will be broken down into named variables shortly
+        DataBox secondBlock; // this block is usually but is not always entirey 0xFF ands its purpose is not clear
+
+        public bool Read(BinaryReader br)
+        {
+            fileNumber.Read(br);
+            firstBlock = new DataBox(0x12);
+            secondBlock = new DataBox(0x82);
+            return true;
+        }
+
+        public bool Write(BinaryWriter bw)
+        {
+            fileNumber.Write(bw);
+            firstBlock.Write(bw);
+            secondBlock.Write(bw);
+            return true;
+        }
+
+    }
+}
---
+++ KPT/Parser/Jump Label Manager/JumpTableInterface.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using KPT.Parser.Elements;
+
+namespace KPT.Parser.Jump_Label_Manager
+{
+    /// <summary>
+    /// Read and write the games jump table
+    /// </summary>
+    /// <remarks>
+    /// The game handles jumps by looking up indexes in a jump table that is stored as a file on disk. 
+    /// </remarks>
+    class JumpTableInterface
+    {
+        private static readonly string JUMP_TABLE_FILE = Path.Combine("PSP_GAME", "USRDIR", "St000", "St000", "St000_SldtDat.bin");
+
+        private short firstNumber;
+        List<JumpTableEntry> jumpTableEntries;
+        DataBox footer;
+
+        public JumpTableInterface()
+        {
+            jumpTableEntries = new List<JumpTableEntry>();
+        }
+
+        public void LoadJumpTable()
+        {
+            string originalFilePath = Path.Combine(ProjectFolder.rootDir, ProjectFolder.unpackedGameFilesDir, JUMP_TABLE_FILE);
+        }
+
+        public void SaveJumpTable()
+        {
+            string targetFilePath = Path.Combine(ProjectFolder.rootDir, ProjectFolder.reassembledGameFilesDir, JUMP_TABLE_FILE);
+        }
+    }
+}

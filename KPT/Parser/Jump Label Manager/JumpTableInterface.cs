af63787fc556fe9cfb32cc0db3fd6e6d970ff21d
dl471 <annamika@bk.ru>
2020-10-18 15:21:26 +0100

    Allow loading of specific jump tables
    -Having the Jump Label Manager hardcoded to use a specific
    jump table in a specific directory was too constraining

--- KPT/Parser/Jump Label Manager/JumpTableInterface.cs
+++ KPT/Parser/Jump Label Manager/JumpTableInterface.cs
@@ -7,6 +7,7 @@ using System.IO;
 using KPT.Parser.Elements;
 using KPT.Parser.Headers;
 using KPT.Parser.Footers;
+using System.Text.RegularExpressions;
 
 namespace KPT.Parser.Jump_Label_Manager
 {
@@ -18,22 +19,67 @@ namespace KPT.Parser.Jump_Label_Manager
     /// </remarks>
     class JumpTableInterface
     {
-        private static readonly string JUMP_TABLE_FILE = Path.Combine("PSP_GAME", "USRDIR", "St000", "St000", "St000_SldtDat.bin");
+        private string jumpTablePath = string.Empty;
 
         St_Header header;
         List<JumpTableEntry> jumpTableEntries;
         St_Footer footer;
 
-        public JumpTableInterface()
+        public JumpTableInterface(string jumpTableLocation)
         {
             header = new St_Header();
             footer = new St_Footer();
             jumpTableEntries = new List<JumpTableEntry>();
+
+            if (IsJumpTable(jumpTableLocation))
+            {
+                jumpTablePath = jumpTableLocation;
+            }
+            else
+            {
+                throw new Exception("File " + jumpTableLocation + "did not match expected jump table name convention");
+            }
+
+        }
+
+        /// <summary>
+        /// Find a jumpt table within a list of files
+        /// </summary>
+        /// <param name="fileList">Array of file paths to check</param>
+        /// <returns>Path to jump table</returns>
+        /// <remarks>Expects to find exactly one jump table, throws exception on finding multiple</remarks>
+        public static string FindJumpTable(string[] fileList)
+        {
+            var jumpTableArray = 
+                fileList
+                .Where(file => IsJumpTable(file))
+                .ToArray();
+
+            switch (jumpTableArray.Length)
+            {
+                case 0:
+                    throw new Exception("Could not find jump table in specified path");
+                    break;
+                case 1:
+                    return jumpTableArray.First();
+                    break;
+                default:
+                    throw new Exception("Found multiple jump tables in specified path - please process each St directory seperately");
+                    break;                
+            }
+
+        }
+
+        public static bool IsJumpTable(string fileName)
+        {
+            Regex regex = new Regex(@"St[0-9A-F]{3}_SldtDat\.bin"); // search for the StXXX_SldtDat.bin format
+            Match match = regex.Match(fileName);
+            return match.Success;
         }
 
         public void LoadJumpTable()
         {
-            string originalFilePath = Path.Combine(ProjectFolder.rootDir, ProjectFolder.unpackedGameFilesDir, JUMP_TABLE_FILE);            
+            string originalFilePath = Path.Combine(ProjectFolder.rootDir, ProjectFolder.unpackedGameFilesDir, jumpTablePath);            
 
             FileStream fs = new FileStream(originalFilePath, FileMode.Open);
             BinaryReader br = new BinaryReader(fs);
@@ -55,7 +101,7 @@ namespace KPT.Parser.Jump_Label_Manager
 
         public void SaveJumpTable()
         {
-            string targetFilePath = Path.Combine(ProjectFolder.rootDir, ProjectFolder.reassembledGameFilesDir, JUMP_TABLE_FILE);
+            string targetFilePath = Path.Combine(ProjectFolder.rootDir, ProjectFolder.reassembledGameFilesDir, jumpTablePath);
 
             FileStream fs = new FileStream(targetFilePath, FileMode.Open);
             BinaryWriter bw = new BinaryWriter(fs);

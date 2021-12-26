4ec1ea87bb31cb415560f903a16df650852220d8
dl471 <dl471kais@gmail.com>
2021-12-26 21:04:41 +0000

    Export InterFileJumps as edge list
    -Find InterFileJumps between files and map them out such that each
    file is a vertex and each jump between files is an edge. Also add
    functionality to export these results as a CSV.

--- KPT/Parser/Branch Mapper/BranchMapper.cs
+++ KPT/Parser/Branch Mapper/BranchMapper.cs
@@ -9,6 +9,9 @@ using KPT.Parser.Headers;
 using KPT.Parser.Elements;
 using KPT.Parser.Jump_Label_Manager;
 using System.Windows.Forms;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.IO;
 using System.Diagnostics;
 
 namespace KPT.Parser.Branch_Mapper
@@ -66,7 +69,110 @@ namespace KPT.Parser.Branch_Mapper
 
         }
 
-        
+        /// <summary>
+        /// A class representing an interfile jump from class A to class B, where the jump will be treated as an edge for graphing purposes
+        /// </summary>
+        public class InterFileEdge
+        {
+
+            public StCpNumber from { get; set; }
+            public StCpNumber to { get; set; }
+            public int frequency { get; set; }
+
+            public InterFileEdge(StCpNumber from, StCpNumber to)
+            {
+                this.from = from;
+                this.to = to;
+                frequency = 1;
+            }
+
+            public override string ToString()
+            {
+                return from.ToString() + "->" + to.ToString();
+            }
+
+            public sealed class InterFileEdgeCSVRecord : ClassMap<InterFileEdge>
+            {
+                public InterFileEdgeCSVRecord()
+                {
+                    Map(csr => csr.from).Name("From");
+                    Map(csr => csr.to).Name("To");
+                    Map(csr => csr.frequency).Name("Frequency");
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Produces a list of interfile jumps between files and the frequency of each jump
+        /// </summary>
+        /// <param name="files">The files to be examined</param>
+        /// <returns>A list of InterFileEdge with the jump data</returns>
+        public List<InterFileEdge> CalcuateEdgesInterFile(List<KCFile> files)
+        {
+            var edgeDict = new Dictionary<string, InterFileEdge>();
+            
+            foreach (var file in files)
+            {
+                foreach (var instruction in file.instructions)
+                {
+                    if (instruction is InterFileJump)
+                    {
+                        var from = (file.header as StCp_Header).GetFileNumber();
+                        var to = (instruction as InterFileJump).fileNumber;
+                        var edge = new InterFileEdge(from, to);
+
+                        InterFileEdge fromDict;
+
+                        bool success = edgeDict.TryGetValue(edge.ToString(), out fromDict);
+
+                        if (success)
+                        {
+                            fromDict.frequency += 1;
+                        }
+                        else
+                        {
+                            edgeDict[edge.ToString()] = edge;
+                        }
+
+                    }
+                }
+            }
+
+            return edgeDict.Values.ToList();
+
+        }
+
+        /// <summary>
+        /// Export list of edges as a CSV for consumption by graphing scripts
+        /// </summary>
+        /// <param name="edges">The edges to export to file</param>
+        /// <param name="filename">The name of the file to export to</param>
+        public void ExportInterFileEdges(List<InterFileEdge> edges, string fileName)
+        {
+
+            FileStream fs = new FileStream(fileName, FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+
+            var csv = new CsvHelper.CsvWriter(sw);
+            csv.Configuration.RegisterClassMap<InterFileEdge.InterFileEdgeCSVRecord>();
+
+            // for some reason writing the header is not working, but it should not matter too much in the end i think
+            //csv.WriteHeader(typeof(InterFileEdge.InterFileEdgeCSVRecord));
+            //csv.NextRecord();
+
+            foreach (var edge in edges)
+            {
+                csv.WriteRecord(edge);
+                csv.NextRecord();
+            }
+
+            csv.Flush();
+
+            sw.Close();
+            fs.Close();
+
+        }
 
     }
 }
